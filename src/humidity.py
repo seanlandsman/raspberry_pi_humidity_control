51b3b878d401e1df15cbeb6ee1ae1a4fbc099527
Sean Landsman <sean@thelandsmans.com>
2017-12-02 11:08:45 +0000

    add adafruit.io logging

--- src/humidity.py
+++ src/humidity.py
@@ -27,7 +27,7 @@ LOG_FILE = '/home/pi/humidity/logs/humidity.log'
 ADAFRUIT_IO_KEY_FILE = 'adafruit.io.key.txt'
 
 # Max time the can be kept on before we shut it down (to cooldown etc)
-MAX_ON_TIME = 15 * 60
+MAX_ON_TIME = 10 * 60
 FAN_REST_TIME = 15 * 60
 
 # Loop check interval
@@ -101,49 +101,53 @@ GPIO.output(EXTRACTOR_FAN_PIN, GPIO.LOW)
 logOutputToFile("GPIO Setup Complete")
 
 while True:
-    ################################################################################
-    # temperature read & logging
-
-    # Attempt to get sensor reading.
-    humidity, temp = readSensorReading(MAX_DHT_READ_ATTEMPTS)
-    if humidity is not None:
-
-        # convert humidity to a percentage
-        humidityPct = float(humidity) / 100
-
-        # calculate the dew point
-        Tdp = calculateDewPointTemperature(humidityPct, temp)
-
-        message = 'Temperature: {0:0.1f} C'.format(temp)
-        message += ', Humidity:    {0:0.1f} %'.format(humidity)
-        message += ', Dew Point:   {0:0.1f} C'.format(Tdp)
-
+    try:
         ################################################################################
-        # fan operation
+        # temperature read & logging
 
-        if fanOn:
-            fanOnTime += LOOP_INTERVAL
-            if fanOnTime >= MAX_ON_TIME:
-                GPIO.output(EXTRACTOR_FAN_PIN, GPIO.LOW)
-                fanOn = False
-                fanOnTime = 0
-                fanRest = True
-                fanRestTime = 0
-                message += ', Turning Fan Off'
-        else:
-            if Tdp < temp + DEW_POINT_RANGE:
-                if fanRest and fanOnTime <= FAN_REST_TIME:
-                    message += ', Fan would turn on - resting'
-                    fanOnTime += LOOP_INTERVAL
-                else:
-                    GPIO.output(EXTRACTOR_FAN_PIN, GPIO.HIGH)
-                    fanOn = True
-                    message += ', Turning Fan On'
+        # Attempt to get sensor reading.
+        humidity, temp = readSensorReading(MAX_DHT_READ_ATTEMPTS)
+        if humidity is not None:
+
+            # convert humidity to a percentage
+            humidityPct = float(humidity) / 100
+
+            # calculate the dew point
+            Tdp = calculateDewPointTemperature(humidityPct, temp)
+
+            message = 'Temperature: {0:0.1f} C'.format(temp)
+            message += ', Humidity:    {0:0.1f} %'.format(humidity)
+            message += ', Dew Point:   {0:0.1f} C'.format(Tdp)
+
+            ################################################################################
+            # fan operation
+
+            if fanOn:
+                fanOnTime += LOOP_INTERVAL
+                if fanOnTime >= MAX_ON_TIME:
+                    GPIO.output(EXTRACTOR_FAN_PIN, GPIO.LOW)
+                    fanOn = False
+                    fanOnTime = 0
                     fanRest = True
                     fanRestTime = 0
+                    message += ', Turning Fan Off'
+            else:
+                if Tdp < temp + DEW_POINT_RANGE:
+                    if fanRest and fanOnTime <= FAN_REST_TIME:
+                        message += ', Fan would turn on - resting'
+                        fanOnTime += LOOP_INTERVAL
+                    else:
+                        GPIO.output(EXTRACTOR_FAN_PIN, GPIO.HIGH)
+                        fanOn = True
+                        message += ', Turning Fan On'
+                        fanRest = True
+                        fanRestTime = 0
 
-        logOutputToFile(message)
-        logOutputToAdafruitIO(temp, humidity, Tdp, fanOn)
+            logOutputToFile(message)
+            logOutputToAdafruitIO(temp, humidity, Tdp, fanOn)
 
-    # sleep until the next temp & fan check
-    time.sleep(LOOP_INTERVAL)
+        # sleep until the next temp & fan check
+        time.sleep(LOOP_INTERVAL)
+    except Exception as err:
+        logOutputToFile("Exception: {0}".format(err))
+        time.sleep(LOOP_INTERVAL)
